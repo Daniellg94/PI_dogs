aa159e75870849cb5dadb4d6b4869cd3dbb90c51
Daniel Lopez <daniel.l1.g@hotmail.com>
2023-06-22 22:41:18 -0500

    deployd

--- client/src/components/Audio/AudioPlayer.jsx
+++ client/src/components/Audio/AudioPlayer.jsx
@@ -1,12 +1,51 @@
-
-import dogs_aut from "./dogs_aut.mp4"
+import React, { useState, useRef } from "react";
+import dogs_aut from "./dogs_aut.mp4";
 
 const AudioPlayer = () => {
+  const [isPlaying, setIsPlaying] = useState(true);
+  const audioRef = useRef(null);
+
+  const handleLoadedMetadata = () => {
+    const audioElement = audioRef.current;
+    if (audioElement) {
+      const duration = audioElement.duration;
+      const startingTime = duration * 0.05;
+      audioElement.currentTime = startingTime;
+    }
+  };
+
+  const togglePlay = () => {
+    const audioElement = audioRef.current;
+    if (audioElement) {
+      if (isPlaying) {
+        audioElement.pause();
+      } else {
+        audioElement.play();
+      }
+      setIsPlaying(!isPlaying);
+    }
+  };
+
+  const setDefaultVolume = () => {
+    const audioElement = audioRef.current;
+    if (audioElement) {
+      audioElement.volume = 0.05; // Establecer el volumen por defecto al 50%
+    }
+  };
 
 
   return (
     <div>
-      <audio src={dogs_aut} controls={true} autoPlay={true} loop={true} />
+      <audio
+        ref={audioRef}
+        src={dogs_aut}
+        controls={false}
+        autoPlay={isPlaying}
+        loop={true}
+        onLoadedMetadata={handleLoadedMetadata}
+        onCanPlay={setDefaultVolume}
+      />
+      <button onClick={togglePlay} style={{background:"none",border:"none",fontSize:"2rem",position:"fixed",top:"93%",left:"1%"}}>{isPlaying ? "🔇" : "🔊"}</button>
     </div>
   );
 };
